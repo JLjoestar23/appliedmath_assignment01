fcdef9c360ceffc666f57de7e6d8c5e70ecdbd5e
Jojo Liu <covenanter32@gmail.com>
2025-09-04 23:40:04 -0400

    implemented secant method solver

---
+++ secant_solver_jojo.m
@@ -0,0 +1,48 @@
+function root_approx = secant_solver_jojo(func, x0, x1, convergence_threshold, max_iter)
+    % basic implementation of the secant method for numerical root finding
+
+    status = 0; % convergence status
+    
+    % initialize x_n and x_prev for the first guess
+    x_n = x1;
+    x_prev = x0;
+    
+    % loop until iterations reached the specified maximum number
+    for i=1:max_iter
+        
+        % break if the update step is too large
+        if abs(func(x_n) - func(x_prev)) > 1/convergence_threshold
+            warning('Updated step size is too large, method failed.');
+            break
+        end
+
+        % calculate the root approximation for the current iteration
+        x_next = x_n - func(x_n)*((x_n - x_prev)/(func(x_n) - func(x_prev)));
+        
+        % check for convergence
+        if abs(x_next - x_n) < convergence_threshold && abs(func(x_n)) < convergence_threshold
+            status = 1; % set status to success
+            break
+        end
+        
+        % update x_n and x_prev for next iteration
+        x_prev = x_n;
+        x_n = x_next;
+
+    end
+    
+    % if reached max number of iterations, status is failed
+    if i == max_iter
+        status = 0;
+    end
+    
+    % if successful, return value of the approximated root
+    if status == 1
+        root_approx = x_n;
+        final_disp = strcat("Root Found, Number of Iterations: ", num2str(i));
+        disp(final_disp);
+    else % warning flag if convergence failed
+        warning("Convergence failed, try different initial guesses or reduce convergence threshold.");
+        root_approx = NaN;
+    end
+end
