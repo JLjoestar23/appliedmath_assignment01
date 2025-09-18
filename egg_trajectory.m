16ab24d85fe52ac59abf6f799346adb5d658418e
Jojo Liu <covenanter32@gmail.com>
2025-09-18 00:17:16 -0400

    adjusted for a more interesting trajectory

--- egg_trajectory.m
+++ egg_trajectory.m
@@ -1,7 +1,7 @@
 % Example parabolic trajectory
 function [x0, y0, theta] = egg_trajectory(t)
     % uses basic kinematic equations to model trajectory
-    x0 = 5*t;
-    y0 = -0.5*9.81*t.^2 + 10;
+    x0 = 6.5*t;
+    y0 = -0.5*9.81*t.^2 + 10*t + 5;
     theta = 10*t;
 end
